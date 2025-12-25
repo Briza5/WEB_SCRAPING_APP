d85e76d13241cd2088bffc8b02a23a3f50625649
Briza5 <brizal555@gmail.com>
2025-12-25 14:04:28 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests # library to handle HTTP requests
+import selectorlib # library to extract data from HTML
+
+url = "https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+def scrape(url):
+    """scrape the page source from the url"""
+    response = requests.get(url, headers=HEADERS) # make a GET request to fetch the raw HTML content
+    source = response.text # extract the text content from the response object
+    return source
+
+if __name__ == "__main__":
+    print(scrape(url))
